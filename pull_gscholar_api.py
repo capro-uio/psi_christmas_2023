7444fb1b1dc5a3635d7be67b90871304382f6563
Inge Amlien <iamlien@gmail.com>
2023-12-11 09:53:53 +0000

    Added append/update json Add tqdm progress

--- pull_gscholar_api.py
+++ pull_gscholar_api.py
@@ -1,9 +1,13 @@
-# %%
 import os
 import json
 from typing import List, Dict, Optional
 from scholarly import scholarly
 import click
+from tqdm import tqdm
+
+
+import logging
+#logging.basicConfig(level=logging.INFO)
 
 def get_single_author(researcher: str) -> Optional[Dict]:
     """
@@ -15,22 +19,33 @@ def get_single_author(researcher: str) -> Optional[Dict]:
     query: List[Dict] = list(scholarly.search_author(researcher))
 
     if not query:
-        print(f"No profiles found for {researcher}.")
+        tqdm.write(f"No profiles found for {researcher}.")
         return None
 
-    # First check if there are more than one author with affiliation 'University of Oslo'
-    query = [q for q in query if q.get('affiliation') == 'University of Oslo']
-    if len(query) == 1:
-        print(f"Found a single profile for '{researcher}' with UiO affiliation, going with this.")
-        return query[0]
+
 
     # Sort authors by 'University of Oslo' affiliation, then by 'citedby'
     query = sorted(query, key=lambda x: (x.get('affiliation') != 'University of Oslo', -x.get('citedby', 0)))
 
-    if len(query) > 1:
-        print(f"Multiple profiles found for '{researcher}'.")
+
+    # # Add ipynb breakpoint:
+    # from IPython.core.debugger import set_trace
+    # set_trace()
+
+    # First check if there are more than one author with affiliation 'University of Oslo'
+    # Check if 'UiO', 'uio', 'Oslo' or 'oslo' is in the affiliation string
+    if len([q for q in query if 'uio' in q.get('affiliation', '').lower() or 'oslo' in q.get('affiliation', '').lower()]) == 1:
+        tqdm.write(f"Found a single profile for '{researcher}' with UiO affiliation, going with this.")
+        return query[0]
+
+    elif len(query) == 1:
+        print(f"Found a single profile for '{researcher}', going with this.")
+        return query[0]
+
+    elif len(query) > 1:
+        tqdm.write(f"Multiple profiles found for '{researcher}'.")
         for i, q in enumerate(query):
-            print(f"{i + 1}. Name: {q.get('name')}, Affiliation: {q.get('affiliation')}, "
+            tqdm.write(f"{i + 1}. Name: {q.get('name')}, Affiliation: {q.get('affiliation')}, "
                   f"Email domain: {q.get('email_domain')}, Cited by: {q.get('citedby')}")
 
         try:
@@ -38,13 +53,14 @@ def get_single_author(researcher: str) -> Optional[Dict]:
             if 1 <= selection <= len(query):
                 return query[selection - 1]
             else:
-                print("Invalid selection number.")
+                tqdm.write("Invalid selection number.")
                 return None
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            tqdm.write("Invalid input. Please enter a number.")
             return None
     else:
-        return query[0]
+        tqdm.write(f"No profiles found for '{researcher}'.")
+        return None
 
 
 def fill_author(author: dict):
@@ -58,7 +74,7 @@ def fill_author(author: dict):
         author_details = scholarly.fill(author)
         return author_details
     else:
-        print("No valid scholar ID found in the author object.")
+        tqdm.write("No valid scholar ID found in the author object.")
         return None
 
 
@@ -72,15 +88,24 @@ def store_data(data: dict, filename: str = 'data/data.json'):
     """
     # Check if the file already exists
     if os.path.exists(filename):
-        overwrite = input(f"The file '{filename}' already exists. Do you want to overwrite it? (yes/no): ").strip().lower()
-        if overwrite not in ['yes', 'y']:
-            print("Operation cancelled. Data not saved.")
+        fileop = input(f"The file '{filename}' already exists. Do you want to append/modify [default], create a new file, or abort? ([append]/overwrite/abort): ").strip().lower()
+        if fileop == 'overwrite':
+            # Save the data
+            with open(filename, 'w', encoding='utf-8') as f:
+                json.dump(data, f, indent=4)
+            tqdm.write(f"Data successfully saved to '{filename}'.")
             return
+        if fileop == 'abort':
+            tqdm.write("Operation cancelled. Data not saved.")
+            return
+        else:
+            # Append the data
+            with open(filename, 'r+', encoding='utf-8') as f:
+                file_data = json.load(f)
+                file_data.update(data)
+                f.seek(0)
+                json.dump(file_data, f, indent=4)
 
-    # Save the data
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f, indent=4)
-    print(f"Data successfully saved to '{filename}'.")
 
 @click.command()
 @click.argument('researchers', nargs=-1)
@@ -92,6 +117,7 @@ def run(researchers: List[str], file_path: str):
     :param researchers: A list of researcher names.
     :param file_path: Path to a file containing researcher names.
     """
+
     if not researchers and not file_path:
         print("No researchers provided. Please provide researchers as arguments or via a file.")
         return
@@ -103,11 +129,11 @@ def run(researchers: List[str], file_path: str):
         with open(file_path, 'r') as file:
             researchers.extend(file.read().splitlines())
 
-    for r in researchers:
-        print("Finding author: ", r)
+    for r in tqdm(researchers, desc="Processing Researchers"):
+        tqdm.write(f"Finding author: {r}")
         selected_author = get_single_author(researcher=r)
         if selected_author:
-            print("Filling data for author: ", selected_author.get('name'))
+            tqdm.write(f"Filling data for author: {selected_author.get('name')}")
             filled_author = fill_author(selected_author)
         else:
             continue
